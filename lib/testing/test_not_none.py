3b276e1af7747106e7a8e34463ce9f193ce859bf
kinsiabdirahman <kinsiabdirahman@outlook.com>
2024-03-09 12:08:58 +0300

    Assignment Completed

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+# testing/test_not_none.py
 
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_return_not_none():
+    '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
+    result = return_not_none()
+    assert result is None, "Expected a None value"
